56f47b71bacc2498864f7a3b32ef145a568ba1b7
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2021-04-15 21:48:22 +0200

    Added database connection

---
+++ Login.Web/Data/Model/LoginDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Web.Data.Model
+{
+    public class LoginDatabaseContext : DbContext
+    {
+        public LoginDatabaseContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
