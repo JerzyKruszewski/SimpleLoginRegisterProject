fed2fa6026f605ea8a698f7583b78a69e4041611
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2021-04-16 21:50:42 +0200

    Added axios api calls

---
+++ client-app/src/api/api.js
@@ -0,0 +1,32 @@
+import { ToastsStore } from "react-toasts";
+var axios = require("axios");
+
+const config = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    }
+  };
+
+const loginRoute = "http://localhost:16560/api/users";
+//axios.default.baseUrl = "http://localhost:16560";
+
+export const login = async (login, password) => {
+    try {
+        return await axios.get(`${loginRoute}/login/${login}/${password}`, config);
+    } catch (error) {
+        ToastsStore.error("Error logging: " + error, 3000, "custom-toast");
+    }
+};
+
+export const register = async (login, password, permissions) => {
+    try {
+        return await axios.post(`${loginRoute}/register`, {login, password, permissions}, config);
+    } catch (error) {
+        ToastsStore.error(
+            "Error registering: " + error,
+            3000,
+            "custom-toast"
+        );
+    } 
+};
